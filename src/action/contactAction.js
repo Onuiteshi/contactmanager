082077b87b0afb48431b623cd1b39fbe0ca0b0d0
onuiteshi <emmanuelonuiteshi@gmail.com>
2020-03-17 15:04:52 +0100

    commit

---
+++ src/action/contactAction.js
@@ -0,0 +1,19 @@
+import {GET_CONTACTS,DELETE_CONTACTS,ADD_CONTACTS} from './type';
+
+export const getContacts = ()=>{
+    return {
+        type:GET_CONTACTS
+    }
+}
+export const deleteContacts = (id)=>{
+    return {
+        type:DELETE_CONTACTS,
+        payload:id
+    }
+}
+export const addContacts = (contact)=>{
+    return {
+        type:ADD_CONTACTS,
+        payload:contact
+    }
+}
